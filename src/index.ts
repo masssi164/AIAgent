0a92e261a9758a4ae581a7cd6590d1551ddcd0ba
masssi164 <95998874+masssi164@users.noreply.github.com>
2024-06-29 12:43:02 +0000

    first communication established

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log(`hello world`)
+import { createOpenAPIChain, ConversationChain } from "langchain/chains";
+import { ChatOpenAI } from "@langchain/openai";
+import {config} from "dotenv"
+config()
+// Initialize the chat model
+const chatModel = new ChatOpenAI({
+  model: "gpt-4o",
+  temperature: 0,
+});
+
+const chain = new ConversationChain({
+    llm:chatModel,
+    outputKey:"output",
+});
+
+const enterChain = async (userQuery:string,calback:(result:string) => void) => {
+    // Create an OpenAPI chain
+    // Run the chain with a specific query
+    const result = await chain.invoke({
+        input:userQuery
+    })
+    calback(result.output)
+}
+
+enterChain("write a letter to my wife",(result) => {
+    console.log(`${result}`)
+})
